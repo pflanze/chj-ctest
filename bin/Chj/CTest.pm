fa90d541d34bc99b4f968a08fda47d8f21779285
Christian Jaeger <ch@christianjaeger.ch>
2021-07-29 03:27:11 +0100

    Add bin/path-to-FIL, proper global name mangling for test symbols

---
+++ bin/Chj/CTest.pm
@@ -0,0 +1,44 @@
+# Copyright 2021 by Christian Jaeger <ch@christianjaeger.ch>
+# Published under the same terms as perl itself
+#
+
+=head1 NAME
+
+Chj::CTest
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
+
+
+package Chj::CTest;
+use strict;
+use utf8;
+use warnings;
+use warnings FATAL => 'uninitialized';
+use experimental 'signatures';
+use Exporter "import";
+
+our @EXPORT=qw();
+our @EXPORT_OK=qw(path_to_FIL);
+our %EXPORT_TAGS=(default => \@EXPORT, all=>[@EXPORT,@EXPORT_OK]);
+
+sub path_to_FIL($path) {
+
+    $path =~ s/\.c\z//
+        or die "need a file with suffix .c, got '$path'";
+
+    $path=~ tr{./-}{D_M};
+
+    $path=~ /^\w+\z/
+        or die "after mangling, path still contains non-word characters: '$path'";
+
+    $path
+}
+
+
+1
--- doc/testing.md
+++ doc/testing.md
@@ -5,12 +5,12 @@
 You use `TEST(shortname) { .... ASSERT_EQ(a,b) ... }`. You can use a short name, it is quasi-local to the file: the file name is being made part of the symbol name, via the Makefile:
 
     target/%.s: %.c
-        cc65 $(CFLAGS) -DFIL=`basename $< .c`_ -o $@ $<
+        cc65 $(CFLAGS) -DFIL=`../chj-ctest/bin/path-to-FIL $<`_ -o $@ $<
 
 Also add the following to the Makefile:
 
     .deps:
-	    bin/gendeps $(OBJS) > .deps
+	    ../chj-ctest/bin/gendeps $(OBJS) > .deps
 
 `make .deps` will then also update `run_tests.c`, i.e. collect all
 TEST cases and build a function that calls all of them. `run_tests.h`
